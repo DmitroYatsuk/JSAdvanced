f85dce7f9b7bccf8d7a915afbd3001c87bf60608
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-09-09 18:46:25 +0300

    New fns and locators have been added

--- HW1_prototypes/js/user.js
+++ HW1_prototypes/js/user.js
@@ -5,13 +5,25 @@ let userForm = function (locators) {
 
 userForm.prototype = {
     initComponent: function () {
+        this.locators.galleryBtn.addEventListener("click", this.goToGalleryHandler);
+        this.locators.galleryBtn.addEventListener("click", this.goToUserHandler);
         this.locators.showPwdBtn.addEventListener("click", this.showPwdHandler);
     },
 
     showPwdHandler: function (e) {
-        this.locators.userPassword.type = this.locators.userPassword.type === 'password'
-            ? 'text' : 'password';
-        this.locators.e.target.innerText = this.locators.e.target.innerText === "Show password"
-            ? 'Hide password' : 'Show password';
+        this.locators.userPassword.type = this.locators.userPassword.type
+            === 'password' ? 'text' : 'password';
+        this.locators.e.target.innerText = this.locators.e.target.innerText
+            === "Show password" ? 'Hide password' : 'Show password';
+    },
+
+    goToGalleryHandler: function (e) {
+        loginForm.hideClass(this.locators.userData);
+        loginForm.showClass(gallery.locators.galleryView);
+    },
+
+    goToUserHandler: function (e) {
+        loginForm.hideClass(this.locators.userData);
+        loginForm.showClass(this.locators.userData);
     }
 }
