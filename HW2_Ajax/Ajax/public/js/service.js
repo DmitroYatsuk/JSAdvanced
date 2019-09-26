e161da1080f888cb53973ab75e9e0dd7f040be8d
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2019-09-26 18:15:05 +0300

    Crete/edit template have been added

--- HW2_Ajax/Ajax/public/js/service.js
+++ HW2_Ajax/Ajax/public/js/service.js
@@ -1,7 +1,7 @@
 
-var listService = (function () {
+class listService {
 
-	function duplicateArray(arr, count) {
+/* 	function duplicateArray(arr, count) {
 		let res = [];
 		for (let i = 0; i <= count; i++) {
 			res = res.concat(arr.map(a => Object.assign({}, a)))
@@ -39,91 +39,49 @@ var listService = (function () {
 
 	function initTooltip() {
 		$('[data-toggle="tooltip"]').tooltip();
+	} */
+
+	editFormTemplate(item) {
+		return `<main class="text-center" role="main">
+		<form id="form-edit" class="form-edit">
+		  <h1 class="h3 mb-3 font-weight-normal">Edit form</h1>
+		  <label for="url" class="sr-only">URL</label>
+		  <input type="text" id="url" class="form-control" placeholder="url" required="" autofocus="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="name" class="sr-only">Name</label>
+		  <input type="text" id="name" class="form-control" placeholder="name" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="description" class="sr-only">Description</label>
+		  <input type="text" id="description" class="form-control" placeholder="description" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="date" class="sr-only">Date</label>
+		  <input type="text" id="date" class="form-control" placeholder="date" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <button class="btn btn-lg btn-primary btn-block" type="submit">Update</button>
+		  <p class="mt-5 mb-3 text-muted">© 2019</p>
+		</form>
+	  </main>`;
 	}
 
-	function tableTemplate(item) {
-		return `<tr>
-		<th scope="row">
-		  <input type="checkbox">
-		</th>
-		<th scope="row">${item.id}</th>
-		<td>${item.name}</td>
-		<td>${item.username}</td>
-		<td>${item.role}</td>
-		<td>${item.email}</td>
-		<td data-toggle="tooltip" title="${item.address.zipcode}, ${item.address.city}, ${item.address.street}">${item.address.city}</td>
-		<td>${item.website}</td>
-		<td>
-			<div class="text-center">
-				<a href="#" data-row-id="${item.id}" class="btn btn-primary btn-sm">Открыть</a>
-	  		</div>
-	  </td>
-	  </tr>`;
-	};
-
-	function detailsTemplate(item) {
-		return `<form class="needs-validation" novalidate="">
-		<div class="row">
-		  <div class="col-md-6 mb-3">
-			<label for="firstName">Имя</label>
-			<input type="text" class="form-control" value="${item.name}" readonly>
-		  </div>
-		  <div class="col-md-6 mb-3">
-			<label for="lastName">Роль</label>
-			<input type="text" class="form-control" value="${item.role}" readonly>
-		  </div>
-		</div>
-		<div class="mb-3">
-		  <label for="username">Логин</label>
-		  <div class="input-group">
-			<div class="input-group-prepend">
-			  <span class="input-group-text">@</span>
-			</div>
-			<input type="text" class="form-control" value="${item.username}" readonly>
-		  </div>
-		</div>
-		<div class="mb-3">
-		  <label for="email">Email <span class="text-muted">(Опционально)</span></label>
-		  <input type="email" class="form-control" value="${item.email}" readonly>
-		</div>
-		<div class="mb-3">
-		  <label for="address">Адрес</label>
-		  <input type="text" class="form-control" value="${item.address.zipcode}, ${item.address.city}, ${item.address.street}" readonly>
-		</div>
-		<div class="mb-3">
-		  <label for="address2">Сайт</label>
-		  <input type="text" class="form-control" value="${item.website}" readonly>
-		</div>
-	  </form>`;
-	};
-
-	function inheritance(parent, child) {
-		let tempChild = child.prototype;
-		child.prototype = Object.create(parent.prototype);
-		child.prototype.constructor = child;
-
-		for (let key in tempChild) {
-			if (tempChild.hasOwnProperty(key)) {
-				child.prototype[key] = tempChild[key];
-			}
-		}
+	createFormTemplate(item) {
+		return `<main class="text-center" role="main">
+		<form id="form-create" class="form-create">
+		  <h1 class="h3 mb-3 font-weight-normal">Edit form</h1>
+		  <label for="url" class="sr-only">URL</label>
+		  <input type="text" id="url" class="form-control" placeholder="url" required="" autofocus="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="name" class="sr-only">Name</label>
+		  <input type="text" id="name" class="form-control" placeholder="name" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="description" class="sr-only">Description</label>
+		  <input type="text" id="description" class="form-control" placeholder="description" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <label for="date" class="sr-only">Date</label>
+		  <input type="text" id="date" class="form-control" placeholder="date" required="" autocomplete="off">
+		  <div class="mb-3"></div>
+		  <button class="btn btn-lg btn-primary btn-block" type="submit">Update</button>
+		  <p class="mt-5 mb-3 text-muted">© 2019</p>
+		</form>
+	  </main>`;
 	}
-
-	return {
-		duplicateArray: duplicateArray,
-		sortEmailAsc: sortEmailAsc,
-		sortEmailDesc: sortEmailDesc,
-		filterAdminRole: filterAdminRole,
-		filterUserRole: filterUserRole,
-		hideElements: hideElements,
-		showElements: showElements,
-		tableTemplate: tableTemplate,
-		detailsTemplate: detailsTemplate,
-		initTooltip: initTooltip,
-		inheritance: inheritance
-	}
-}());
-
-
-//listService.setLogPass({l:"a@.com",p:"1234"})
-//listService.initTooltip();
+}
